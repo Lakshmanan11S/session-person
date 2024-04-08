9bce880830be4d14823254a8b1ebe8dcc44d5674
Lakshmanan11S <lakshmanan@elonnativesystem.com>
2024-04-08 17:42:49 +0530

    inital

--- router/route.js
+++ router/route.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const router = express.Router()
 const usercontroler = require ('../controler/control.js')
 
+
 router.post('/post',usercontroler.create)
 router.post('/login',usercontroler.login)
 router.post('/logout',usercontroler.logout)
